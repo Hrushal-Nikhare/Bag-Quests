319d29992bd5c85dc68c3ca28a1790ef1c164a4f
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2024-08-17 17:12:24 +0530

    added some slash cmds

--- Main.py
+++ Main.py
@@ -5,6 +5,24 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+# The echo command simply echoes on command
+@app.command("/test")
+def repeat_text(ack, respond, command):
+    # Acknowledge command request
+    ack()
+    respond(f"{command['text']}")
+
+@app.command("/start")
+def start(ack, respond, command):
+    ack()
+    respond("Starting the quest!")
+
+@app.command("/ping")
+def ping(ack, respond, command):
+    ack()
+    respond("Pong")
+
+
 # Start your app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
